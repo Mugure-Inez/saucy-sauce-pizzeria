82abb4365cc39f2804324a3c1aaff0fd6784469b
Mugure-Inez <enezmugure@gmail.com>
2021-03-08 07:22:16 +0300

    small changes made to webpage outlook

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
-var small = 600;
-var medium = 1000;
-var large = 1400;
-// document.getElementById("small").innerHTML = 600;
-
-if (pizza-size === "small")
-{
-    
-}
+function getCart() {
+    pizzaSize = "";
+    pizzaCrust = "";
+    pizzaTopping = "";
+    var total = 0;
+    var sizeTotal = 0;
+    var sizaArray = document.getElementById("pizza-crust");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizaArray[i].value;
+            pizzaCrust = pizzaCrust+selectedSize+"<br>";
+        }
+    }
+    if (selectedSize === "small"){
+        sizeTotal = 6;
+        pizzaSize = pizzaSize+sizeTotal+"<br>";
+    } else if (selectedSize === "medium") {
+        sizeTotal = 10;
+        pizzaSize = pizzaSize+sizeTotal+"<br>";
+    } else if (selectedSize === "large") {
+        sizeTotal = 14;
+        pizzaSize = pizzaSize+sizeTotal+"<br>";
+    }
+    total = sizeTotal;
+    getCrust(total, pizzaSize, pizzaTopping, pizzaCrust);
+};
+function getCrust(total, pizzaSize, pizzaTopping, pizzaCrust) {
+	var crustTotal = 0;
+	var selectedCrust;
+	var crustArray = document.getElementById("pizza-Crust");
+	for (var j = 0; j < crustArray.length; j++) {
+		if (crustArray[j].checked) {
+			selectedCrust = crustArray[j].value;
+			pizzaCrust = pizzaCrust + selectedCrust + "<br>";
+		}
+		if (selectedCrust === "Cheese Stuffed Crust") {
+			crustTotal = 3;
+		}
+	}
+	total = (total + crustTotal);
+	pizzaSize = pizzaSize + crustTotal + "<br>";
+	getSize(Total,pizzaSize,pizzaTopping);
+};
